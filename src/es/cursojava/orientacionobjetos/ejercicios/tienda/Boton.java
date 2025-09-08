9831e98023d6001b6eb4dbc25544e38a040a6808
isGoodSoup <sargent.pointed453@passinbox.com>
2025-09-08 17:55:11 +0200

    feat: objetos, ejercicio 4

---
+++ src/es/cursojava/orientacionobjetos/ejercicios/tienda/Boton.java
@@ -0,0 +1,10 @@
+package es.cursojava.orientacionobjetos.ejercicios.tienda;
+
+public class Boton {
+	
+	private String color;
+	private int tamanho;
+	private String forma;
+	
+	
+}
---
+++ src/es/cursojava/orientacionobjetos/ejercicios/tienda/TiendaManiqui.java
@@ -0,0 +1,5 @@
+package es.cursojava.orientacionobjetos.ejercicios.tienda;
+
+public class TiendaManiqui {
+
+}
