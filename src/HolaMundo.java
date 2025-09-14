af591f6a96698d51bf1d42159e17fb86ef25f778
isGoodSoup <sargent.pointed453@passinbox.com>
2025-09-14 19:21:16 +0200

    feat: NameGen.java

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,3 +1,5 @@
+import es.cursojava.utils.NameGen;
+
 public class HolaMundo {
     
     public static void main( String[] args ) {
---
+++ src/es/cursojava/utils/NameGen.java
@@ -0,0 +1,76 @@
+package es.cursojava.utils;
+
+import java.util.Random;
+
+public class NameGen {
+	
+	public static String Name() {
+		Random random = new Random();
+		String name = "";
+		String[] first = {
+	            "Alice", "Alejandro", 
+	            "Ben", "Beatriz", 
+	            "Charlie", "Carmen", 
+	            "Daniel", "Dolores", 
+	            "Emily", "Eduardo", 
+	            "Frank", "Fernanda", 
+	            "Grace", "Guillermo", 
+	            "Henry", "Helena", 
+	            "Isla", "Ignacio", 
+	            "Jack", "Jimena", 
+	            "Karen", "Kevin", 
+	            "Liam", "Lucía", 
+	            "Mia", "Mateo", 
+	            "Noah", "Natalia", 
+	            "Olivia", "Oscar", 
+	            "Paul", "Pilar", 
+	            "Quinn", "Queralt", 
+	            "Rachel", "Raúl", 
+	            "Sophie", "Sergio", 
+	            "Thomas", "Tatiana", 
+	            "Ursula", "Ulises", 
+	            "Victor", "Valeria", 
+	            "Wendy", "Wilmer", 
+	            "Xander", "Ximena", 
+	            "Yasmin", "Yago", 
+	            "Zoe", "Zacarías"
+	    };
+
+	    String[] last = {
+	            "Anderson", "Alonso", 
+	            "Brown", "Barrios", 
+	            "Carter", "Castillo", 
+	            "Davis", "Díaz", 
+	            "Evans", "Esparza", 
+	            "Foster", "Fernández", 
+	            "Garcia", "González", 
+	            "Harris", "Herrera", 
+	            "Ingram", "Iglesias", 
+	            "Johnson", "Jiménez", 
+	            "King", "Keller", 
+	            "Lopez", "Luna", 
+	            "Martinez", "Moreno", 
+	            "Nelson", "Navarro", 
+	            "Ortiz", "Oliver", 
+	            "Parker", "Pérez", 
+	            "Quinn", "Quijano", 
+	            "Robinson", "Ramírez", 
+	            "Smith", "Santos", 
+	            "Taylor", "Torres", 
+	            "Underwood", "Urbina", 
+	            "Valentine", "Vargas", 
+	            "Walker", "Wenceslao", 
+	            "Xavier", "Ximénez", 
+	            "Young", "Ybarra", 
+	            "Zimmerman", "Zamora"
+	    };
+	    
+	    int num = random.nextInt(first.length) + 1;
+	    int num2 = random.nextInt(last.length) + 1;
+	    String fnlfirst = first[num];
+	    String fnllast = last[num2];
+	    name = fnlfirst + " " + fnllast;
+		
+	    return name;
+	}
+}
