986e8786a2c9673ba146b3c654a7e516a4e04ade
isGoodSoup <sargent.pointed453@passinbox.com>
2025-09-16 17:59:16 +0200

    feat: herencia

---
+++ src/es/cursojava/orientacionobjetos/herencia/Animal.java
@@ -0,0 +1,46 @@
+package es.cursojava.orientacionobjetos.herencia;
+
+public class Animal {
+	private String nombre;
+	private double peso;
+	private long id;
+	
+	public Animal() {
+		
+	}
+	
+	public Animal(String nombre, double peso, long id) {
+		super();
+		this.nombre = nombre;
+		this.peso = peso;
+		this.id = id;
+	}
+	
+	public void comer() {
+		System.out.println("El animal " + this.nombre + " está comiendo");
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+}
---
+++ src/es/cursojava/orientacionobjetos/herencia/Vaca.java
@@ -0,0 +1,5 @@
+package es.cursojava.orientacionobjetos.herencia;
+
+public class Vaca extends Animal {
+	
+}
