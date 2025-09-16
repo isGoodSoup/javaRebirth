9f261be4844c06a5be1ceeb1fb5e30b07ba68b81
isGoodSoup <sargent.pointed453@passinbox.com>
2025-09-16 19:30:18 +0200

    feat: herencia*

---
+++ src/es/cursojava/orientacionobjetos/herencia/Lobo.java
@@ -0,0 +1,31 @@
+package es.cursojava.orientacionobjetos.herencia;
+
+public class Lobo extends Animal {
+	private int numColmillos;
+	
+	public Lobo() {
+		
+	}
+
+	public Lobo(String nombre, double peso, long id) {
+		super(nombre, peso, id);
+	}
+
+	public Lobo(int numColmillos) {
+		super();
+		this.numColmillos = numColmillos;
+	}
+	
+	public void comer() {
+		System.out.println("El lobo está cazando");
+		super.comer();
+	}
+
+	public int getNumColmillos() {
+		return numColmillos;
+	}
+
+	public void setNumColmillos(int numColmillos) {
+		this.numColmillos = numColmillos;
+	}
+}
--- src/es/cursojava/orientacionobjetos/herencia/Zoologico.java
+++ src/es/cursojava/orientacionobjetos/herencia/Zoologico.java
@@ -4,5 +4,7 @@ public class Zoologico {
 	
 	public static void main(String[] args) {
 		Vaca vaca = new Vaca("Wendolin", 500, 2, 2);
+		Lobo lobo = new Lobo("Miguel", 50, 222);
+		lobo.comer();
 	}
 }
