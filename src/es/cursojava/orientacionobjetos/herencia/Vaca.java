5cf2e5f426a5dbff658a272df6f7fbd2b18e3b81
isGoodSoup <sargent.pointed453@passinbox.com>
2025-09-16 18:27:32 +0200

    fix: herencia*

--- src/es/cursojava/orientacionobjetos/herencia/Vaca.java
+++ src/es/cursojava/orientacionobjetos/herencia/Vaca.java
@@ -1,5 +1,26 @@
 package es.cursojava.orientacionobjetos.herencia;
 
 public class Vaca extends Animal {
+	private int numCuernos;
+
+	public Vaca() {
 	
+	}
+
+	public Vaca(String nombre, double peso, 
+			long id, int numCuernos) {
+		super(nombre, peso, id);
+	}
+	
+	public void mugir() {
+		System.out.println("El animal " + this.getNombre() + " está mugiendo");
+	}
+
+	public int getNumCuernos() {
+		return numCuernos;
+	}
+
+	public void setNumCuernos(int numCuernos) {
+		this.numCuernos = numCuernos;
+	}
 }
---
+++ src/es/cursojava/orientacionobjetos/herencia/Zoologico.java
@@ -0,0 +1,8 @@
+package es.cursojava.orientacionobjetos.herencia;
+
+public class Zoologico {
+	
+	public static void main(String[] args) {
+		Vaca vaca = new Vaca("Wendolin", 500, 2, 0);
+	}
+}
