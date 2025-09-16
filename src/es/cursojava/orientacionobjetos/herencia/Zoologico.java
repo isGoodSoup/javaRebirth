692dce43d1806b3eb36d160b9f75616eccbf08d3
isGoodSoup <sargent.pointed453@passinbox.com>
2025-09-16 19:12:53 +0200

    fix: variable en herencia

--- src/es/cursojava/orientacionobjetos/herencia/Zoologico.java
+++ src/es/cursojava/orientacionobjetos/herencia/Zoologico.java
@@ -3,6 +3,6 @@ package es.cursojava.orientacionobjetos.herencia;
 public class Zoologico {
 	
 	public static void main(String[] args) {
-		Vaca vaca = new Vaca("Wendolin", 500, 2, 0);
+		Vaca vaca = new Vaca("Wendolin", 500, 2, 2);
 	}
 }
