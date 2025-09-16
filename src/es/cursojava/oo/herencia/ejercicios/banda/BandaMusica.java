5077d98feb2bb9423157535452e0c2421c4d339c
isGoodSoup <sargent.pointed453@passinbox.com>
2025-09-16 20:29:06 +0200

    feat: herencia, banda de música ejercicio 4*

--- src/es/cursojava/oo/herencia/ejercicios/banda/BandaMusica.java
+++ src/es/cursojava/oo/herencia/ejercicios/banda/BandaMusica.java
@@ -3,7 +3,11 @@ package es.cursojava.oo.herencia.ejercicios.banda;
 public class BandaMusica {
 	
 	public void main(String[] args) {
-		Instrumento i1 = new Instrumento();
+		Instrumento i1 = new Instrumento("guitarra", "guitarra acústica", true);
+		Instrumento i2 = new Instrumento("eléctrica", "guitarra eléctrica", true);
+		Instrumento i3 = new Instrumento("piano", "piano de cola", true);
+		Instrumento i4 = new Instrumento("tambor", "tambor", true);
+		Instrumento[] orquesta = {i1, i2, i3, i4};
 	}
 	
 	public void afinarInstrumentos() {
