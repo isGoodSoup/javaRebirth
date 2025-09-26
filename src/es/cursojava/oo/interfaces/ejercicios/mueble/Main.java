ee6faf6e424ec91653cf1ec9d82a7b19e001b934
isGoodSoup <sargent.pointed453@passinbox.com>
2025-09-26 20:13:37 +0200

    feat: interfaces ejercicio 1*

--- src/es/cursojava/oo/interfaces/ejercicios/mueble/Main.java
+++ src/es/cursojava/oo/interfaces/ejercicios/mueble/Main.java
@@ -1,11 +1,20 @@
 package es.cursojava.oo.interfaces.ejercicios.mueble;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+	private final static Logger log = LoggerFactory.getLogger(new Object() {}.getClass().getEnclosingClass());
+	
 	public static void main(String[] args) {
-		
+		Main m = new Main();
+		m.desinfectarObjetos();
 	}
 	
 	public void desinfectarObjetos() {
-		
+		Mueble mesa = new Mesa("");
+		Mueble silla = new Silla("");
+		Verdura lechuga = new Lechuga("");
+		Verdura judias = new JudiasVerdes("", true);
 	}
 }
