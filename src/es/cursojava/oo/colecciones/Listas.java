efaf5da9328da3f527bb10fcaa9ebd01f223e8e0
isGoodSoup <sargent.pointed453@passinbox.com>
2025-10-03 16:21:31 +0200

    faet: listas y colecciones

---
+++ src/es/cursojava/oo/colecciones/Listas.java
@@ -0,0 +1,26 @@
+package es.cursojava.oo.colecciones;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import es.cursojava.oo.Persona;
+
+public class Listas {
+	
+	public static void main(String[] args) {
+		List<Persona> lista = new ArrayList<Persona>();
+//		lista.add("Amarillo");
+//		lista.add(100.5);
+		for (int i = 0; i < 10000; i++) {
+			lista.add(new Persona(null, null, null, i));
+		}
+		System.out.println(lista.size());
+
+		for (Object object : lista) {
+			if(object instanceof Persona) {
+				Persona p = (Persona)object;
+				p.getNombre();
+			}
+		}
+	}
+}
