a427c49da27550de042944c9d0ab044d67b41880
isGoodSoup <sargent.pointed453@passinbox.com>
2025-11-07 16:34:12 +0100

    feat: excepciones ejercicio 2

--- src/es/cursojava/oo/excepciones/ejercicios/ejercicio2/Main.java
+++ src/es/cursojava/oo/excepciones/ejercicios/ejercicio2/Main.java
@@ -26,16 +26,22 @@ public class Main implements Executable {
 	    Dexter.toGetString("DATOS DEL ALUMNO");
 	    List<Alumno> alumnos = new ArrayList<>();
 	    while(alumnos.size() < 6) {
-	    	try {
-		    	nombre = Dexter.toScan("Nombre");
-		    	dni = Dexter.toScan("DNI");
-		    	edad = Dexter.toScanInt("Edad");
-		    	nota = Dexter.toScanDouble("Nota media");
-		    	Alumno a = new Alumno(nombre, dni, edad, nota);
-		    	alumnos.add(a);
-		    } catch(NotaInvalidaException e) {
-		    	System.err.println(e.getMessage());
-		    }
+	    	Dexter.toGetString("ALUMNO " + alumnos.size());
+	    	nombre = Dexter.toScan("Nombre");
+	    	dni = Dexter.toScan("DNI");
+	    	edad = Dexter.toScanInt("Edad");
+
+	    	boolean notaValida = false;
+	        while (!notaValida) {
+		    	try {
+		    		nota = Dexter.toScanDouble("Nota media");
+			    	Alumno a = new Alumno(nombre, dni, edad, nota);
+			    	alumnos.add(a);
+			    	notaValida = true;
+			    } catch(NotaInvalidaException e) {
+			    	System.err.println(e.getClass().getSimpleName() + ": " + e.getMessage());
+			    }
+	        }
 	    }
 	    return alumnos;
 	}
