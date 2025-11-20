9340cee07eeb28b5e4a4fed4fa6a0625414dccaf
isGoodSoup <sargent.pointed453@passinbox.com>
2025-11-20 17:27:20 +0100

    feat: hibernate*

--- src/es/cursojava/oo/hibernate/empresa/Empresa.java
+++ src/es/cursojava/oo/hibernate/empresa/Empresa.java
@@ -1,9 +1,82 @@
 package es.cursojava.oo.hibernate.empresa;
 
+import java.util.Date;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "TB_EMPRESA")
 public class Empresa {
+	@Id
+	@Column(name = "ID")
 	private long id;
+	@Column(length = 30)
 	private String nombre;
+	@Column(name = "SOCIEDAD")
 	private String sociedad;
+	@Column(name = "CIF")
 	private String cif;
+	@Column(name = "ACTIVIDAD")
+	private String actividad;
+	@Column(name = "FECHA")
+	private Date fecha;
 	
+	public Empresa(long id, String nombre, String sociedad, String cif,
+			String actividad, Date fecha) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.sociedad = sociedad;
+		this.cif = cif;
+		this.actividad = actividad;
+		this.fecha = fecha;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getSociedad() {
+		return sociedad;
+	}
+
+	public void setSociedad(String sociedad) {
+		this.sociedad = sociedad;
+	}
+
+	public String getCif() {
+		return cif;
+	}
+
+	public void setCif(String cif) {
+		this.cif = cif;
+	}
+
+	public String getActividad() {
+		return actividad;
+	}
+
+	public void setActividad(String actividad) {
+		this.actividad = actividad;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
 }
