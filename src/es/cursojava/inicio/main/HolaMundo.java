132ff4de4cb5ef9fc823462002f9c6b235a15204
Tardes <Tardes@GET-07>
2025-12-10 20:16:03 +0100

    update: miercoles 10-12, 20:15

--- src/es/cursojava/inicio/main/HolaMundo.java
+++ src/es/cursojava/inicio/main/HolaMundo.java
@@ -1,9 +1,17 @@
 package es.cursojava.inicio.main;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+import com.filereader.utils.FileReader;
 import com.loggerfactory.main.AppLogger;
 
+import utils.Dexter;
+
 public class HolaMundo {
-    
+    private static Scanner scan = new Scanner(System.in);
+	
     public static void main( String[] args ) {
         System.out.println("Hola Mundo");
 
@@ -17,5 +25,16 @@ public class HolaMundo {
         
         AppLogger logger = new AppLogger(5000);
         logger.init();
+        
+        FileReader reader = new FileReader();
+        try {
+			List<String> lineas = reader.readFile("./don-quijote.txt");
+			for (String linea : lineas) {
+				System.out.print(linea);
+				scan.nextLine();
+			}
+		} catch (IOException e) {
+			Dexter.printException(e);
+		}
     }
 }
